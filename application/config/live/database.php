36bc85d76b68c1c64dadeda68f70591c1dd90efc
Rich Lovelock <rikkiola@gmail.com>
2012-11-17 17:46:35 +0000

    Live config files

---
+++ application/config/live/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'   => 'mysql',
+			'host'     => 'tunnel.pagodabox.com',
+			'database' => 'tgc',
+			'username' => 'maple',
+			'password' => 'KNInXSOa',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+		),
+
+	),
+
+);
