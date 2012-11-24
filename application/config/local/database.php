330bbd40ec7fcb20d8c55f6ea477bf5808240316
Rich Lovelock <rikkiola@gmail.com>
2012-11-24 20:21:54 +0000

    Adding local config

---
+++ application/config/local/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'   => 'mysql',
+			'host'     => 'localhost',
+			'database' => 'tgc',
+			'username' => 'tgcuser',
+			'password' => 'n1ckf4ld0',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+		),
+
+	),
+
+);
