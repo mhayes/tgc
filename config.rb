b0e4b21ef9198a52482ba2d35c7818fb3304af4e
Rich Lovelock <rikkiola@gmail.com>
2012-12-05 23:58:31 +0000

    Compass config changes

--- config.rb
+++ config.rb
@@ -1,13 +1,20 @@
 require 'zurb-foundation'
 # Require any additional compass plugins here.
 
+#Misc
+http_path 									= "public"
 
-# Set this to the root of your project when deployed:
-http_path 		= "public"
-css_dir 		= "public/css"
-sass_dir 		= "public/sass"
-images_dir 		= "public/img"
-javascripts_dir = "public/js"
+#Styles
+css_dir 										= "public/css"
+sass_dir 										= "public/sass"
+
+#Images
+images_dir 									= "public/img"
+generated_images_path				= "public/img/sprites"
+http_generated_images_path 	= "/img/sprites"
+
+#Javascript
+javascripts_dir 						= "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -17,7 +24,6 @@ javascripts_dir = "public/js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
